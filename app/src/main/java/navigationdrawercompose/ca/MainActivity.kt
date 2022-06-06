71b1b7df404745101d510eb8bfb174e742088925
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-06-06 11:44:52 -0400

    studying

--- app/src/main/java/navigationdrawercompose/ca/MainActivity.kt
+++ app/src/main/java/navigationdrawercompose/ca/MainActivity.kt
@@ -3,27 +3,74 @@ package navigationdrawercompose.ca
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import navigationdrawercompose.ca.ui.DrawerBody
+import navigationdrawercompose.ca.ui.DrawerHeader
+import navigationdrawercompose.ca.ui.MenuItem
 import navigationdrawercompose.ca.ui.theme.NavigationDrawerComposeTheme
-import navigationdrawercompose.ca.ui.theme.Typography
+import navigationdrawercompose.ca.ui.AppBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NavigationDrawerComposeTheme {
-
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                         AppBar(
+                             onNavigationIconClick = {
+                                 scope.launch {
+                                     scaffoldState.drawerState.open()
+                                 }
+                             }
+                         )
+                    },
+                    drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+                    drawerContent = {
+                        DrawerHeader()
+                        DrawerBody(
+                            items = listOf(
+                                MenuItem(
+                                    id = "home",
+                                    title = "Home",
+                                    contentDescription = "Go to Home screen",
+                                    icon = Icons.Default.Home
+                                ),
+                                MenuItem(
+                                    id = "profile",
+                                    title = "Profile",
+                                    contentDescription = "Go to Profile screen",
+                                    icon = Icons.Default.AccountBox
+                                ),
+                                MenuItem(
+                                    id = "settings",
+                                    title = "Settings",
+                                    contentDescription = "Go to Settings screen",
+                                    icon = Icons.Default.Settings
+                                ),
+                            ),
+                            onItemClick = {
+                                /*when(it.id){
+                                    "settings"
+                                }*/
+                                println("Clicked on ${it.title}")
+                            }
+                        )
+                    }
+                ) {
+                    
+                }
             }
         }
     }
---
+++ app/src/main/java/navigationdrawercompose/ca/ui/MenuItem.kt
@@ -0,0 +1,10 @@
+package navigationdrawercompose.ca.ui
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem(
+      val id: String,
+      val title: String,
+      val contentDescription: String,
+      val icon: ImageVector
+)
